28c4509e3dd99fd5b597f90e7535c6c74eac81e6
mrweijianli <leevi1044@outlook.com>
2022-10-30 22:28:50 +0000

    resnet and densenet experiments on different types of graphs

---
+++ densenet_alg.py
@@ -0,0 +1,494 @@
+# This implementation is based on the DenseNet-BC implementation in torchvision
+# https://github.com/pytorch/vision/blob/master/torchvision/models/densenet.py
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as cp
+from collections import OrderedDict
+import time
+import os, shutil, csv
+import numpy as np
+import pandas as pd
+import cv2
+
+testdf = pd.read_csv('/home/weijian-volume/weijian/weijiandl/git/GraphMaster/data/annotation/train.txt')
+
+testdf = testdf.to_numpy()
+
+TEST_DIR = '/home/weijian-volume/weijian/weijiandl/git/GraphMaster/data/images/'
+
+train_imgs = []
+train_labels = []
+
+for i in range(len(testdf)):
+    
+    if os.path.exists(TEST_DIR + testdf[i][0]):
+        img = cv2.imread(TEST_DIR + testdf[i][0], cv2.IMREAD_COLOR)
+#         img = cv2.imread(TEST_DIR + testdf[i][0], cv2.IMREAD_GRAYSCALE)
+        if img is not None :
+#             print(TEST_DIR + testdf[i][0])
+            img = cv2.resize(img, (112, 112), interpolation=cv2.INTER_AREA).reshape(112, 112, 3)
+
+        #     img = image.load_img(TEST_DIR + testdf[i][0], target_size=(224, 224))
+        #     x = image.img_to_array(img)
+        #     x = np.expand_dims(x, axis=0)
+        #     x = preprocess_input(x)
+
+            train_imgs.append(img)
+            if testdf[i][1] == ' Algorithm':
+                train_labels.append(1)
+            else:
+                train_labels.append(0)
+
+# train_imgs = np.array(train_imgs)
+# train_labels = np.array(train_labels)
+
+p = np.random.permutation(len(train_imgs))
+train_imgs = np.array(train_imgs)[p]
+train_labels = np.array(train_labels)[p]
+
+
+testset = pd.read_csv('/home/weijian-volume/weijian/weijiandl/git/GraphMaster/data/annotation/test.txt')
+
+testset = testset.to_numpy()
+
+IMAGE_DIR = '/home/weijian-volume/weijian/weijiandl/git/GraphMaster/data/images/'
+
+
+test_imgs = []
+test_labels = []
+
+for i in range(len(testset)):
+    
+    if os.path.exists(IMAGE_DIR + testset[i][0]):
+        img = cv2.imread(IMAGE_DIR + testset[i][0], cv2.IMREAD_COLOR)
+#         img = cv2.imread(IMAGE_DIR + testset[i][0], cv2.IMREAD_GRAYSCALE)
+        if img is not None :
+#             print(IMAGE_DIR + testset[i][0])
+            img = cv2.resize(img, (112, 112), interpolation=cv2.INTER_AREA).reshape(112, 112, 3)
+
+        #     img = image.load_img(IMAGE_DIR + testset[i][0], target_size=(224, 224))
+        #     x = image.img_to_array(img)
+        #     x = np.expand_dims(x, axis=0)
+        #     x = preprocess_input(x)
+
+            test_imgs.append(img)
+            if testset[i][1] == ' Algorithm':
+                test_labels.append(1)
+            else:
+                test_labels.append(0)
+
+test_imgs = np.array(test_imgs)
+test_labels = np.array(test_labels)
+
+
+
+
+def _bn_function_factory(norm, relu, conv):
+    def bn_function(*inputs):
+        concated_features = torch.cat(inputs, 1)
+        bottleneck_output = conv(relu(norm(concated_features)))
+        return bottleneck_output
+
+    return bn_function
+
+
+class _DenseLayer(nn.Module):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate, efficient=False):
+        super(_DenseLayer, self).__init__()
+        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size * growth_rate,
+                        kernel_size=1, stride=1, bias=False)),
+        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
+        self.add_module('relu2', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+                        kernel_size=3, stride=1, padding=1, bias=False)),
+        self.drop_rate = drop_rate
+        self.efficient = efficient
+
+    def forward(self, *prev_features):
+        bn_function = _bn_function_factory(self.norm1, self.relu1, self.conv1)
+        if self.efficient and any(prev_feature.requires_grad for prev_feature in prev_features):
+            bottleneck_output = cp.checkpoint(bn_function, *prev_features)
+        else:
+            bottleneck_output = bn_function(*prev_features)
+        new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
+        return new_features
+
+
+class _Transition(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features):
+        super(_Transition, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(num_input_features))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
+                                          kernel_size=1, stride=1, bias=False))
+        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+
+
+class _DenseBlock(nn.Module):
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate, efficient=False):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(
+                num_input_features + i * growth_rate,
+                growth_rate=growth_rate,
+                bn_size=bn_size,
+                drop_rate=drop_rate,
+                efficient=efficient,
+            )
+            self.add_module('denselayer%d' % (i + 1), layer)
+
+    def forward(self, init_features):
+        features = [init_features]
+        for name, layer in self.named_children():
+            new_features = layer(*features)
+            features.append(new_features)
+        return torch.cat(features, 1)
+
+
+class DenseNet(nn.Module):
+    r"""Densenet-BC model class, based on
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 3 or 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+            (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+        small_inputs (bool) - set to True if images are 32x32. Otherwise assumes images are larger.
+        efficient (bool) - set to True to use checkpointing. Much more memory efficient, but slower.
+    """
+    def __init__(self, growth_rate=12, block_config=(16, 16, 16), compression=0.5,
+                 num_init_features=24, bn_size=4, drop_rate=0,
+                 num_classes=10, small_inputs=True, efficient=False):
+
+        super(DenseNet, self).__init__()
+        assert 0 < compression <= 1, 'compression of densenet should be between 0 and 1'
+
+        # First convolution
+        if small_inputs:
+            self.features = nn.Sequential(OrderedDict([
+                ('conv0', nn.Conv2d(3, num_init_features, kernel_size=3, stride=1, padding=1, bias=False)),
+            ]))
+        else:
+            self.features = nn.Sequential(OrderedDict([
+                ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ]))
+            self.features.add_module('norm0', nn.BatchNorm2d(num_init_features))
+            self.features.add_module('relu0', nn.ReLU(inplace=True))
+            self.features.add_module('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1,
+                                                           ceil_mode=False))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(
+                num_layers=num_layers,
+                num_input_features=num_features,
+                bn_size=bn_size,
+                growth_rate=growth_rate,
+                drop_rate=drop_rate,
+                efficient=efficient,
+            )
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = _Transition(num_input_features=num_features,
+                                    num_output_features=int(num_features * compression))
+                self.features.add_module('transition%d' % (i + 1), trans)
+                num_features = int(num_features * compression)
+
+        # Final batch norm
+        self.features.add_module('norm_final', nn.BatchNorm2d(num_features))
+
+        # Linear layer
+        self.classifier = nn.Linear(num_features, num_classes)
+
+        # Initialization
+        for name, param in self.named_parameters():
+            if 'conv' in name and 'weight' in name:
+                n = param.size(0) * param.size(2) * param.size(3)
+                param.data.normal_().mul_(math.sqrt(2. / n))
+            elif 'norm' in name and 'weight' in name:
+                param.data.fill_(1)
+            elif 'norm' in name and 'bias' in name:
+                param.data.fill_(0)
+            elif 'classifier' in name and 'bias' in name:
+                param.data.fill_(0)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
+    
+
+class AverageMeter(object):
+    """
+    Computes and stores the average and current value
+    Copied from: https://github.com/pytorch/examples/blob/master/imagenet/main.py
+    """
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def train_epoch(model, loader, optimizer, epoch, n_epochs, print_freq=1):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    error = AverageMeter()
+
+    # Model on train mode
+    model.train()
+
+    end = time.time()
+    for batch_idx, (input, target) in enumerate(loader):
+        # Create vaiables
+        target = target.type(torch.LongTensor)
+        if torch.cuda.is_available():
+            input = input.cuda()
+            target = target.cuda()
+
+        # compute output
+        output = model(input)
+        loss = torch.nn.functional.cross_entropy(output, target)
+
+        # measure accuracy and record loss
+        batch_size = target.size(0)
+        target = torch.stack([target, 1-target], dim=1)
+        # _, pred = output.data.cpu().topk(1, dim=1)
+        # error.update(torch.ne(pred.squeeze(), target.cpu()).float().sum().item() / batch_size, batch_size)
+        # losses.update(loss.item(), batch_size)
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # print stats
+        if batch_idx % print_freq == 0:
+            res = '\t'.join([
+                'Epoch: [%d/%d]' % (epoch + 1, n_epochs),
+                'Iter: [%d/%d]' % (batch_idx + 1, len(loader)),
+                'Time %.3f (%.3f)' % (batch_time.val, batch_time.avg),
+                'Loss %.4f (%.4f)' % (losses.val, losses.avg),
+                'Error %.4f (%.4f)' % (error.val, error.avg),
+            ])
+            print(res)
+
+    # Return summary statistics
+    return batch_time.avg, losses.avg, error.avg
+
+
+def test_epoch(model, loader, print_freq=1, is_test=True):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    error = AverageMeter()
+
+    # Model on eval mode
+    model.eval()
+    preds = []
+
+    end = time.time()
+    with torch.no_grad():
+        for batch_idx, (input, target) in enumerate(loader):
+            target = target.type(torch.LongTensor)
+            # Create vaiables
+            if torch.cuda.is_available():
+                input = input.cuda()
+                target = target.cuda()
+
+            # compute output
+            output = model(input)
+            pred = output
+            preds.append(pred)
+
+            loss = torch.nn.functional.cross_entropy(output, target)
+
+            # measure accuracy and record loss
+            batch_size = target.size(0)
+            _, pred = output.data.cpu().topk(1, dim=1)
+            error.update(torch.ne(pred.squeeze(), target.cpu()).float().sum().item() / batch_size, batch_size)
+            losses.update(loss.item(), batch_size)
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            # print stats
+            # if batch_idx % print_freq == 0:
+            #     res = '\t'.join([
+            #         'Test' if is_test else 'Valid',
+            #         'Iter: [%d/%d]' % (batch_idx + 1, len(loader)),
+            #         'Time %.3f (%.3f)' % (batch_time.val, batch_time.avg),
+            #         'Loss %.4f (%.4f)' % (losses.val, losses.avg),
+            #         'Error %.4f (%.4f)' % (error.val, error.avg),
+            #     ])
+            #     print(res)
+
+    # Return summary statistics
+    return loss, preds
+
+
+def train(model, train_set, valid_set=None, test_set=None, save="./", n_epochs=300,
+          batch_size=64, lr=0.1, wd=0.0001, momentum=0.9, seed=None):
+    if seed is not None:
+        torch.manual_seed(seed)
+
+    # Data loaders
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True,
+                                               pin_memory=(not torch.cuda.is_available()), num_workers=0)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False,
+                                              pin_memory=(torch.cuda.is_available()), num_workers=0)
+    if valid_set is None:
+        valid_loader = None
+    else:
+        valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=batch_size, shuffle=False,
+                                                   pin_memory=(not torch.cuda.is_available()), num_workers=0)
+    # Model on cuda
+    if torch.cuda.is_available():
+        model = model.cuda()
+
+    # Wrap model for multi-GPUs, if necessary
+    model_wrapper = model
+    if torch.cuda.is_available() and torch.cuda.device_count() > 1:
+        model_wrapper = torch.nn.DataParallel(model).cuda()
+
+    # Optimizer
+    optimizer = torch.optim.SGD(model_wrapper.parameters(), lr=lr, momentum=momentum, nesterov=True, weight_decay=wd)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0.5 * n_epochs, 0.75 * n_epochs],
+                                                     gamma=0.1)
+
+    # Start log
+    with open(os.path.join(save, 'results.csv'), 'w') as f:
+        f.write('epoch,train_loss,train_error,valid_loss,valid_error,test_error\n')
+
+    # Train model
+    best_error = 1
+    for epoch in range(n_epochs):
+        _, train_loss, train_error = train_epoch(
+            model=model_wrapper,
+            loader=train_loader,
+            optimizer=optimizer,
+            epoch=epoch,
+            n_epochs=n_epochs,
+        )
+        scheduler.step()
+        _, valid_loss, valid_error, _ = test_epoch(
+            model=model_wrapper,
+            loader=valid_loader if valid_loader else test_loader,
+            is_test=(not valid_loader)
+        )
+
+        # Determine if model is the best
+        if valid_loader:
+            if valid_error < best_error:
+                best_error = valid_error
+                print('New best error: %.4f' % best_error)
+                torch.save(model.state_dict(), os.path.join(save, 'model.dat'))
+        else:
+            torch.save(model.state_dict(), os.path.join(save, 'model.dat'))
+
+        # Log results
+        with open(os.path.join(save, 'results.csv'), 'a') as f:
+            f.write('%03d,%0.6f,%0.6f,%0.5f,%0.5f,\n' % (
+                (epoch + 1),
+                train_loss,
+                train_error,
+                valid_loss,
+                valid_error,
+            ))
+
+    # Final test of model on test set
+    model.load_state_dict(torch.load(os.path.join(save, 'model.dat')))
+    if torch.cuda.is_available() and torch.cuda.device_count() > 1:
+        model = torch.nn.DataParallel(model).cuda()
+    test_results = test_epoch(
+        model=model,
+        loader=test_loader,
+        is_test=True
+    )
+    _, _, test_error, preds = test_results
+    preds = torch.cat(preds, dim=0)
+    with open(os.path.join(save, 'results.csv'), 'a') as f:
+        f.write(',,,,,%0.5f\n' % (test_error))
+    print('Final test error: %.4f' % test_error)
+
+    return preds
+    
+
+class ImgDataset:
+    
+    def __init__(self, imgs, labels):
+        self.imgs = imgs
+        self.labels = labels
+    
+    def __getitem__(self, idx):
+        x = self.imgs[idx]
+        h = x.shape[0]
+        w = x.shape[1]
+        c = x.shape[2]
+        x = x.reshape(c, h, w)
+
+        return x.astype(np.float32), self.labels[idx].astype(np.float32)
+    
+    def __len__(self):
+        return len(self.imgs)
+    
+model = DenseNet(
+    growth_rate=12,
+    num_init_features=112,
+    num_classes=2,
+    small_inputs=True,
+    efficient=False,
+)
+# train_data = ImgDataset(train_imgs, train_labels)
+valid_data = None
+test_data = ImgDataset(test_imgs, test_labels)
+
+# tst_preds = train(model=model, train_set=train_data, valid_set=valid_data, test_set=test_data, save="./",
+#                 n_epochs=30, batch_size=8, seed=1)
+
+# temporary code for test only
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False,
+                                              pin_memory=(not torch.cuda.is_available()), num_workers=0)
+model.load_state_dict(torch.load(os.path.join("./", 'model.dat')))
+if torch.cuda.is_available() and torch.cuda.device_count() > 1:
+    model = torch.nn.DataParallel(model).cuda()
+model = model.cuda()
+test_results = test_epoch(
+    model=model,
+    loader=test_loader,
+    is_test=True
+)
+_, tst_preds = test_results
+tst_preds = torch.cat(tst_preds, dim=0)
+
+from torchmetrics import Accuracy
+accuracy = Accuracy()
+print("accuracy: ")
+print(accuracy(tst_preds.detach().cpu(), torch.Tensor(test_labels).long()))
---
+++ run_resnet_alg.py
@@ -0,0 +1,222 @@
+import os, shutil, csv
+import numpy as np
+import pandas as pd
+import cv2
+import optuna
+
+import tensorflow as tf
+from keras.models import Model, Sequential
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras import layers, models
+from keras.layers import Flatten, Dense, Conv2D, Input, ZeroPadding2D, AveragePooling2D, BatchNormalization, Activation, add
+
+
+def objective(trial):
+  testdf = pd.read_csv('/home/weijian-volume/weijian/weijiandl/git/GraphMaster/data/annotation/train.txt')
+
+  testdf = testdf.to_numpy()
+
+  TEST_DIR = '/home/weijian-volume/weijian/weijiandl/git/GraphMaster/data/images/'
+
+  # OUTPUT_DIR = '../DocFigure_dataset/GraphPlots'
+
+  # if not os.path.exists(OUTPUT_DIR):
+  #     os.mkdir(OUTPUT_DIR)
+      
+  # for file in graphplotlist:
+  #     shutil.copy(file, OUTPUT_DIR)
+
+  # prepare training
+  train_imgs = []
+  train_labels = []
+
+  for i in range(len(testdf)):
+      
+      if os.path.exists(TEST_DIR + testdf[i][0]):
+          img = cv2.imread(TEST_DIR + testdf[i][0], cv2.IMREAD_COLOR)
+  #         img = cv2.imread(TEST_DIR + testdf[i][0], cv2.IMREAD_GRAYSCALE)
+          if img is not None :
+  #             print(TEST_DIR + testdf[i][0])
+              img = cv2.resize(img, (112, 112), interpolation=cv2.INTER_AREA).reshape(112, 112, 3)
+
+          #     img = image.load_img(TEST_DIR + testdf[i][0], target_size=(224, 224))
+          #     x = image.img_to_array(img)
+          #     x = np.expand_dims(x, axis=0)
+          #     x = preprocess_input(x)
+
+              train_imgs.append(img)
+              if testdf[i][1] == ' Algorithm':
+                train_labels.append(1)
+              else:
+                train_labels.append(0)
+              # if testdf[i][1] == ' Graph plots':
+              #     train_labels.append([1, 0, 0, 0])
+              # elif testdf[i][1] == ' Tables':
+              #     train_labels.append([0, 1, 0, 0])
+              # elif testdf[i][1] == ' Bar plots':
+              #     train_labels.append([0, 0, 1, 0])
+              # elif testdf[i][1] == ' Algorithm':
+              #     train_labels.append([0, 0, 0, 1])
+              # else:
+              #     train_labels.append([0, 0, 0, 0])
+  # train_imgs = np.array(train_imgs)
+  # train_labels = np.array(train_labels)
+
+  p = np.random.permutation(len(train_imgs))
+  train_imgs = np.array(train_imgs)[p]
+  train_labels = np.array(train_labels)[p]
+
+
+  # define model
+  def conv_bn(x, nb_filters, kernel_size, padding="same", strides=(1, 1), name=None):
+      if name is not None:
+          bn_name = name + "_bn"
+          conv_name = name + "_conv"
+      else:
+          bn_name = None
+          conv_name = None
+      x = Conv2D(nb_filters, kernel_size, padding=padding, strides=strides, name=conv_name)(x)
+
+      x = BatchNormalization(axis=-1, name=bn_name)(x)
+      x = Activation('relu')(x)
+
+      return x
+
+
+  def bottle_block18(input, nb_filters, padding="same", strides=(1, 1), with_conv_shortcut=False):
+      k1, k2 = nb_filters
+      x = conv_bn(input, k1, (3, 3), padding=padding, strides=strides)
+      x = conv_bn(x, k2, (3, 3), padding=padding)
+      if with_conv_shortcut:
+          shortcut = conv_bn(input, k2, (1, 1), padding=padding, strides=strides)
+          x = add([x, shortcut])
+      else:
+          x = add([x, input])
+      return x
+
+  def resNet_customized(height, width, channels):
+      input = Input(shape=(height, width, channels))
+
+      x = input
+
+      x = ZeroPadding2D((1, 1))(input)
+      x = conv_bn(x, nb_filters=64, kernel_size=(3, 3), padding="valid", strides=(1, 1))
+
+      x = bottle_block18(x, (64, 64), strides=(1, 1), with_conv_shortcut=True)
+      x = bottle_block18(x, (64, 64), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (64, 64), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (64, 64), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (64, 64), strides=(1, 1), with_conv_shortcut=False)
+
+      x = bottle_block18(x, (128, 128), strides=(2, 2), with_conv_shortcut=True)
+      x = bottle_block18(x, (128, 128), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (128, 128), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (128, 128), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (128, 128), strides=(1, 1), with_conv_shortcut=False)
+
+      x = bottle_block18(x, (256, 256), strides=(2, 2), with_conv_shortcut=True)
+      x = bottle_block18(x, (256, 256), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (256, 256), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (256, 256), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (256, 256), strides=(1, 1), with_conv_shortcut=False)
+
+      x = bottle_block18(x, (512, 512), strides=(2, 2), with_conv_shortcut=True)
+      x = bottle_block18(x, (512, 512), strides=(1, 1), with_conv_shortcut=False)
+      x = bottle_block18(x, (512, 512), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (512, 512), strides=(1, 1), with_conv_shortcut=False)
+      # x = bottle_block18(x, (512, 512), strides=(1, 1), with_conv_shortcut=False)
+
+      x = AveragePooling2D((7, 7))(x)
+
+      x = Flatten()(x)
+      # x = Dense(128, activation="relu")(x)
+      output = Dense(1, activation='sigmoid')(x)
+
+      model = Model(inputs=input, outputs=output)
+
+      return model
+
+  model = resNet_customized(112, 112, 3)
+
+  model.summary()
+
+  model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=trial.suggest_float("lr", 0, 1)),loss='binary_crossentropy',metrics=['accuracy'])
+
+  # history = model.fit(train_imgs, train_labels, validation_split=0.3, epochs=30)
+  history = model.fit(train_imgs, train_labels, batch_size=trial.suggest_int("batch_size", 0, 70), epochs=20)
+  model.save("./resNet_customized34_bar")
+
+  # testing
+  testset = pd.read_csv('/home/weijian-volume/weijian/weijiandl/git/GraphMaster/data/annotation/test.txt')
+
+  testset = testset.to_numpy()
+
+  IMAGE_DIR = '/home/weijian-volume/weijian/weijiandl/git/GraphMaster/data/images/'
+
+
+  test_imgs = []
+  test_labels = []
+
+  for i in range(len(testset)):
+      
+      if os.path.exists(IMAGE_DIR + testset[i][0]):
+          img = cv2.imread(IMAGE_DIR + testset[i][0], cv2.IMREAD_COLOR)
+  #         img = cv2.imread(IMAGE_DIR + testset[i][0], cv2.IMREAD_GRAYSCALE)
+          if img is not None :
+  #             print(IMAGE_DIR + testset[i][0])
+              img = cv2.resize(img, (112, 112), interpolation=cv2.INTER_AREA).reshape(112, 112, 3)
+
+          #     img = image.load_img(IMAGE_DIR + testset[i][0], target_size=(224, 224))
+          #     x = image.img_to_array(img)
+          #     x = np.expand_dims(x, axis=0)
+          #     x = preprocess_input(x)
+
+              test_imgs.append(img)
+              if testset[i][1] == ' Algorithm':
+                test_labels.append(1)
+              else:
+                test_labels.append(0)
+              # if testset[i][1] == ' Graph plots':
+              #     test_labels.append([1, 0, 0, 0])
+              # elif testset[i][1] == ' Tables':
+              #     test_labels.append([0, 1, 0, 0])
+              # elif testset[i][1] == ' Bar plots':
+              #     test_labels.append([0, 0, 1, 0])
+              # elif testset[i][1] == ' Algorithm':
+              #     test_labels.append([0, 0, 0, 1])
+              # else:
+              #     test_labels.append([0, 0, 0, 0])
+
+  test_imgs = np.array(test_imgs)
+  test_labels = np.array(test_labels)
+  loss, accuracy= model.evaluate(test_imgs, test_labels, verbose=False)
+  print(accuracy)
+
+  with open("acc_alg.txt", 'a') as f:
+    f.write(str(accuracy))
+    f.write("\n")
+
+  return accuracy
+
+def main():
+  search_space = {
+      "lr": [1e-3],
+      "batch_size": [32]
+  }
+
+  study = optuna.create_study(sampler=optuna.samplers.GridSampler(search_space), direction="maximize")
+  study.optimize(objective)
+
+  best = study.best_trial
+  best_acc = best.value
+  print(f"best accuracy: {best_acc}")
+  print(f"best hparams: ")
+  for key, value in best.params.items():
+    print("    {}: {}".format(key, value))
+
+
+
+if __name__ == "__main__":
+  main()
